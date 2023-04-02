7520a2789cde49d281ac40e2a318e1b1f180e109
pione3r <dev2diver@gmail.com>
2023-04-02 17:02:26 +0900

    feat: 채팅 init

--- back/app.js
+++ back/app.js
@@ -13,24 +13,22 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
-  socket.on("enter room", (userName) => {
+  socket.once("enter room", (userName) => {
     socket.join("room1");
+
     console.log(`${userName} 님이 입장하셨습니다.`);
-
     io.to("room1").emit("notice enter", `${userName} 님이 입장하셨습니다.`);
-  });
 
-  socket.on("leave room", (userName) => {
-    console.log(`${userName} 님이 나가셨습니다.`);
+    socket.on("new message", ({ userName, message }) =>
+      io
+        .to("room1")
+        .emit("new message", { userName: userName, message: message })
+    );
 
-    io.to("room1").emit("notice leave", `${userName} 님이 나가셨습니다.`);
-
-    socket.leave("room1");
-  });
-
-  socket.on("new message", (message) => {
-    console.log(message);
-    io.to("room1").emit("new message", message);
+    socket.on("disconnect", () => {
+      console.log(`${userName} 님이 나가셨습니다.`);
+      io.to("room1").emit("notice leave", `${userName} 님이 나가셨습니다.`);
+    });
   });
 });
 
--- front/src/App.js
+++ front/src/App.js
@@ -2,24 +2,30 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 export default function App() {
-  const [socket, setSocket] = useState(io("http://localhost:8000"));
+  const [socket] = useState(io("http://localhost:8000"));
 
-  const [user, setUser] = useState("");
+  const [userName, setUserName] = useState("");
 
   const [message, setMessage] = useState("");
 
-  const [messageList, setMessageList] = useState([]);
+  const [messageLog, setMessageLog] = useState([]);
 
   useEffect(() => {
-    setSocket(io("http://localhost:8000"));
+    return () => socket.disconnect();
   }, []);
 
-  console.log(messageList);
-
   useEffect(() => {
-    socket.on("notice enter", (notice) => {
-      console.log(notice);
-      setMessageList((prev) => [...prev, notice]);
+    socket.on("notice enter", (notice) =>
+      setMessageLog((prev) => [...prev, notice])
+    );
+
+    socket.on("notice leave", (notice) =>
+      setMessageLog((prev) => [...prev, notice])
+    );
+
+    socket.on("new message", (message) => {
+      setMessageLog((prev) => [...prev, message]);
+      setMessage("");
     });
   }, [socket]);
 
@@ -27,61 +33,72 @@ export default function App() {
     <>
       <button
         onClick={() => {
-          if (user !== "") return;
-
+          socket.connect();
           const userName = makeRandomName();
-          setUser(userName);
-
           socket.emit("enter room", userName);
+          setUserName(userName);
         }}
       >
         들어가기
       </button>
       <button
         onClick={() => {
-          if (user === "") return;
-
-          const userName = user;
-
-          socket.emit("leave room", userName);
-          socket.on("notice leave", (notice) => {
-            setMessageList(() => [notice]);
-          });
-
-          setUser("");
+          socket.disconnect();
         }}
       >
         나가기
       </button>
       <div>
         <input
-          placeholder="새 메시지 입력"
           value={message}
           onChange={(event) => setMessage(event.target.value)}
         />
         <button
           onClick={() => {
-            socket.emit("new message", message);
-            setMessage("");
+            socket.emit("new message", {
+              userName: userName,
+              message: message,
+            });
           }}
         >
           메시지 보내기
         </button>
       </div>
       <div>
-        {messageList.map((message, index) => (
-          <div key={index}>{message}</div>
-        ))}
+        메시지 로그
+        <div>
+          {messageLog.map((message, index) => (
+            <div key={index}>
+              {message.userName ? (
+                <>
+                  <div>{message.userName}</div>
+                  <div>{message.message}</div>
+                </>
+              ) : (
+                <div>{message}</div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
 }
 
 function makeRandomName() {
-  let name = "";
-  let possible = "abcdefghijklmnopqrstuvwxyz";
-  for (let i = 0; i < 3; i++) {
-    name += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return name;
+  var 감정 = ["미운", "고마운", "사랑하는", "증오하는", "끔찍한", "무서운"];
+
+  var 색상 = ["빨강", "노랑", "주황", "파랑", "초록", "하양", "까망"];
+
+  var 자연 = ["숲", "나무", "꽃", "풀", "바다", "절벽"];
+
+  var 단어 = ["날개", "번개", "해", "달", "팬티", "모자"];
+
+  return (
+    감정[Math.floor(Math.random() * 감정.length)] +
+    " 나의 " +
+    색상[Math.floor(Math.random() * 색상.length)] +
+    "색 " +
+    단어[Math.floor(Math.random() * 단어.length)]
+  );
 }
